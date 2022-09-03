1db34c52f42f840d0f763f2810741d29a71dd0f3
Igor Kuptsov <108464106+imercury13@users.noreply.github.com>
2022-09-03 15:09:13 +0300

    Переход на pyproject.toml

--- setup.py
+++ setup.py
@@ -1,32 +1,6 @@
-from setuptools import setup, find_packages
-from os.path import join, dirname
-import yandex_oauth
+from setuptools import setup
 
 setup(
-    name='yandex_oauth',
-    version=yandex_oauth.__version__,
-    packages=find_packages(),
-    description='Yandex OAuth Lib',
-    long_description=open(join(dirname(__file__), 'README.rst')).read(),
-    author=yandex_oauth.__author__,
-    author_email='ya360@uh.net.ru',
-    maintainer=yandex_oauth.__author__,
-    maintainer_email='ya360@uh.net.ru',
-    download_url='https://github.com/imercury13/yandex_oauth',
-    #url='https://ya360.uh.net.ru',
-    license='MIT',
-    project_urls={
-        "Documentation": "https://yandex-oauth.readthedocs.io/ru/1.1.2",
-        "Bug Tracker": "https://github.com/imercury13/yandex_oauth/issues"
-    },
-    classifiers=[
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Programming Language :: Python'
-    ],
-    install_requires=[
-        'jreq>=1.1.0',
-    ],
+    
     include_package_data=True,
 )
